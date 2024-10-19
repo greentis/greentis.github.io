f2e171d70794e5c82feec12eb51f4266597e522e
greentis <lklcs2004@gmail.com>
2024-10-20 00:46:44 +0800

    light & dark mode

--- navbar.js
+++ navbar.js
@@ -1,10 +1,34 @@
 Vue.createApp({
+    computed: {
+        navbarClasses(){
+            return{
+                'navbar-light':!this.useDarkNavBar,
+                'bg-light':!this.useDarkNavBar,
+                'navbar-dark':this.useDarkNavBar,
+                'bg-dark':this.useDarkNavBar
+            }
+        }
+    },
     data(){
         return {
+            activePage: 0,
+            useDarkNavBar: false,
             pages:[
-                {text:"Home", url:"index.html"},
-                {text:"About", url:"index.html"},
-                {text:"Blog", url:"index.html"},
+                {
+                    tab: {text:"Home", url:"index.html"},
+                    title: "Home Page",
+                    content: "Name: Lau Kin Lun\nDegree: Computer Science"
+                },
+                {
+                    tab: {text:"About", url:"index.html"},
+                    title: "About Page",
+                    content: "This page is made with vue.js"
+                },
+                {
+                    tab: {text:"Blog", url:"index.html"},
+                    title: "Blog Page",
+                    content: "Path"
+                }
             ],
             dropdowns:[
                 {text:"Html exercise", pages:[
@@ -16,4 +40,8 @@ Vue.createApp({
             ]
         }
     }
-}).mount("#navbarNav");
+}).mount("body");
+
+document.getElementById("dark-mode-toggle").onchange = ()=>{
+    console.log(document.getElementById("dark-mode-toggle").value);
+};
